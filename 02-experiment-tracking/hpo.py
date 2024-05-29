84cb9e9f3f85da0955fac64aca24aaf259093774
AyowoleT <techytape@gmail.com>
2024-05-29 23:00:55 +0000

    update

--- 02-experiment-tracking/hpo.py
+++ 02-experiment-tracking/hpo.py
@@ -34,13 +34,19 @@ def run_optimization(data_path: str, num_trials: int):
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
     def objective(params):
+        with mlflow.start_run():
 
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+            mlflow.set_tag("developer", "ayowole")
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
+            mlflow.log_metric("rmse", rmse)
+            mlflow.log_params(params)
 
-        return {'loss': rmse, 'status': STATUS_OK}
+            return {'loss': rmse, 'status': STATUS_OK}
+
+        mlflow.log_artifact(local_path="models/lin_reg.bin", artifact_path="artifacts")
 
     search_space = {
         'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
